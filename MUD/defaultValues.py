29853738d65732acb6c4e49258f67fe5c044e1f4
CDHayden <chayden50@gmail.com>
2021-04-29 18:05:59 +0100

    Format

--- MUD/defaultValues.py
+++ MUD/defaultValues.py
@@ -27,14 +27,23 @@ game_settings_defaults_mapper = {
     SettingType.minimum: settings.MIN_TRAIT_VALUE,
 }
 
-def get_game_setting(value, setting_type):
-    return getattr(settings, value, None) or game_settings_defaults_mapper[setting_type]
 
-DEFAULT_GOLD_VALUE = get_game_setting("DEFAULT_GOLD_VALUE", SettingType.default)
+def get_game_setting(value, setting_type):
+    return (
+        getattr(settings, value, None)
+        or game_settings_defaults_mapper[setting_type]
+    )
+
+
+DEFAULT_GOLD_VALUE = get_game_setting(
+    "DEFAULT_GOLD_VALUE", SettingType.default
+)
 MIN_GOLD_VALUE = get_game_setting("MIN_GOLD_VALUE", SettingType.minimum)
 MAX_GOLD_VALUE = get_game_setting("MAX_GOLD_VALUE", SettingType.maximum)
 
-DEFAULT_POINTS_VALUE = get_game_setting("DEFAULT_POINTS_VALUE", SettingType.default)
+DEFAULT_POINTS_VALUE = get_game_setting(
+    "DEFAULT_POINTS_VALUE", SettingType.default
+)
 MIN_POINTS_VALUE = get_game_setting("MIN_POINTS_VALUE", SettingType.minimum)
 MAX_POINTS_VALUE = get_game_setting("MAX_POINTS_VALUE", SettingType.maximum)
 
@@ -48,18 +57,38 @@ MIN_MP_VALUE = get_game_setting("MIN_MP_VALUE", SettingType.minimum)
 MAX_MP_VALUE = get_game_setting("MAX_MP_VALUE", SettingType.maximum)
 
 
-DEFAULT_STRENGTH_VALUE = get_game_setting("DEFAULT_STRENGTH_VALUE", SettingType.default)
-MIN_STRENGTH_VALUE = get_game_setting("MIN_STRENGTH_VALUE", SettingType.minimum)
-MAX_STRENGTH_VALUE = get_game_setting("MAX_STRENGTH_VALUE", SettingType.maximum)
+DEFAULT_STRENGTH_VALUE = get_game_setting(
+    "DEFAULT_STRENGTH_VALUE", SettingType.default
+)
+MIN_STRENGTH_VALUE = get_game_setting(
+    "MIN_STRENGTH_VALUE", SettingType.minimum
+)
+MAX_STRENGTH_VALUE = get_game_setting(
+    "MAX_STRENGTH_VALUE", SettingType.maximum
+)
 
-DEFAULT_AGILITY_VALUE = get_game_setting("DEFAULT_AGILITY_VALUE", SettingType.default)
+DEFAULT_AGILITY_VALUE = get_game_setting(
+    "DEFAULT_AGILITY_VALUE", SettingType.default
+)
 MIN_AGILITY_VALUE = get_game_setting("MIN_AGILITY_VALUE", SettingType.minimum)
 MAX_AGILITY_VALUE = get_game_setting("MAX_AGILITY_VALUE", SettingType.maximum)
 
-DEFAULT_DEXTERITY_VALUE = get_game_setting("DEFAULT_DEXTERITY_VALUE", SettingType.default)
-MIN_DEXTERITY_VALUE = get_game_setting("MIN_DEXTERITY_VALUE", SettingType.minimum)
-MAX_DEXTERITY_VALUE = get_game_setting("MAX_DEXTERITY_VALUE", SettingType.maximum)
+DEFAULT_DEXTERITY_VALUE = get_game_setting(
+    "DEFAULT_DEXTERITY_VALUE", SettingType.default
+)
+MIN_DEXTERITY_VALUE = get_game_setting(
+    "MIN_DEXTERITY_VALUE", SettingType.minimum
+)
+MAX_DEXTERITY_VALUE = get_game_setting(
+    "MAX_DEXTERITY_VALUE", SettingType.maximum
+)
 
-DEFAULT_INVENTORY_SIZE = get_game_setting("DEFAULT_INVENTORY_SIZE", SettingType.default)
-MIN_INVENTORY_SIZE = get_game_setting("MIN_INVENTORY_SIZE", SettingType.minimum)
-MAX_INVENTORY_SIZE = get_game_setting("MAX_INVENTORY_SIZE", SettingType.maximum)
+DEFAULT_INVENTORY_SIZE = get_game_setting(
+    "DEFAULT_INVENTORY_SIZE", SettingType.default
+)
+MIN_INVENTORY_SIZE = get_game_setting(
+    "MIN_INVENTORY_SIZE", SettingType.minimum
+)
+MAX_INVENTORY_SIZE = get_game_setting(
+    "MAX_INVENTORY_SIZE", SettingType.maximum
+)
