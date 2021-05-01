4b735d69f5c0433db46cb7be20d1627e86c4fc4f
CDHayden <chayden50@gmail.com>
2021-05-01 21:49:57 +0100

    Cache static files on AWS

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -168,6 +168,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+            'expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+            'CacheControl': 'max-age=96408000',
+            }
+
     AWS_STORAGE_BUCKET_NAME = 'arbaya'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID= os.environ.get('AWS_ACCESS_KEY_ID')
